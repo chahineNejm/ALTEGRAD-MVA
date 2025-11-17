b92da53ab57f44519a1d95dc6acd96635acd20c8
unknown <chahine.nejma@gmail.com>
2025-11-17 17:36:44 +0100

    importing material for lab 4

---
+++ Lab4/part2/code_lab_community_detection.py
@@ -0,0 +1,62 @@
+"""
+Graph Mining - ALTEGRAD - Nov 2024
+"""
+
+import networkx as nx
+import numpy as np
+from scipy.sparse.linalg import eigs
+from scipy.sparse import diags, eye
+from random import randint
+from sklearn.cluster import KMeans
+
+
+
+############## Task 3
+# Perform spectral clustering to partition graph G into k clusters
+def spectral_clustering(G, k):
+    
+    ##################
+    # your code here #
+    ##################
+    
+    
+    
+    return clustering
+
+
+############## Task 4
+
+##################
+# your code here #
+##################
+
+
+
+
+############## Task 5
+# Compute modularity value from graph G based on clustering
+def modularity(G, clustering):
+    
+    ##################
+    # your code here #
+    ##################
+    
+    
+    
+    
+    return modularity
+
+
+
+############## Task 6
+
+##################
+# your code here #
+##################
+
+
+
+
+
+
+
